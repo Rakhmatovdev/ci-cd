502ff9b672e9d0c0a7403062c8c2d99ed7a0e61a
Jasur <rakhmatovjasur3@gmail.com>
2024-05-21 18:39:04 +0500

    Actual

---
+++ helpers/sum.js
@@ -0,0 +1,15 @@
+// const numbers = [1, 2, 3, 4];
+
+// const value = 'salom'
+// console.log(value);
+
+// function sum() {
+//   for (const iterator of numbers) {
+//     console.log(iterator);
+//   }
+// }
+// sum(),
+
+export const sum=(a,b)=>{
+return  a+b
+}
---
+++ helpers/sum.test.js
@@ -0,0 +1,5 @@
+import {sum} from "./sum"
+
+
+test("test sum",()=>expect(sum(4,5)).toBe(9))
+
